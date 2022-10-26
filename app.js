346c0e879adf9eeef9a04d3700b29033f0f77789
Lina <akarabutova59@gmail.com>
2022-10-26 09:38:15 +0300

    add a win/lose functionality

--- app.js
+++ app.js
@@ -1,24 +1,40 @@
 function getRandomValue(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
+const START = "Game is started";
+const P_ATTACK = "Player attacked";
+const M_ATTACK = "Monster attacked";
+const GAME_OVER = "Game is over.";
 const app = Vue.createApp({
   data() {
     return {
       playerHealth: 100,
       monsterHealth: 100,
       currentRound: 0,
+      winner: null,
+      actions: [],
     };
   },
   methods: {
+    startGame() {
+      this.playerHealth = 100;
+      this.monsterHealth = 100;
+      this.currentRound = 0;
+      this.winner = null;
+      this.actions.push(START);
+    },
     attackMonster() {
       this.currentRound++;
       const attackVal = getRandomValue(5, 12);
       this.monsterHealth -= attackVal;
+      actions.push(`${P_ATTACK} with the damage ${attackVal}`);
       this.attackPlayer();
     },
     attackPlayer() {
       const attackVal = getRandomValue(8, 15);
       this.playerHealth = this.playerHealth - attackVal;
+      actions.push(`${M_ATTACK} with the damage ${attackVal}`);
+
       console.log(attackVal, this.playerHealth);
     },
     specailAttackMonster() {
@@ -28,12 +44,14 @@ const app = Vue.createApp({
       this.attackPlayer();
     },
     healPlayer() {
+      this.currentRound++;
       const healVal = getRandomValue(8, 20);
       if (this.playerHealth + healVal > 100) {
         this.playerHealth = 100;
       } else {
         this.playerHealth += healVal;
       }
+      this.attackPlayer();
     },
   },
   computed: {
@@ -45,6 +63,24 @@ const app = Vue.createApp({
     },
     mayUseSpecailAttack() {
       return this.currentRound % 3 !== 0;
+    },
+  },
+  watch: {
+    playerHealth(val) {
+      if (val <= 0 && this.monsterHealth <= 0) {
+        //it's a draw
+        this.winner = "draw";
+      } else if (val <= 0) {
+        this.winner = "monster";
+      }
+    },
+    monsterHealth(val) {
+      if (val <= 0 && this.monsterHealth <= 0) {
+        //it's a draw
+        this.winner = "draw";
+      } else if (val <= 0) {
+        this.winner = "player";
+      }
     },
   },
 });
